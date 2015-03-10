036feb4e6cd4a9985ebe3467de4da65009e5dbcb
Larionov Vadim <larionov.vadim@mail.ru>
2015-03-10 12:24:19 +0300

    Revert "Removed Languages.java"
    This reverts commit cf1637a1fbfd57b8bfe62745304966c4f6bcf872.

--- app/src/main/java/ru/tech_mail/translator/TranslatorActivity.java
+++ app/src/main/java/ru/tech_mail/translator/TranslatorActivity.java
@@ -41,11 +41,7 @@ public class TranslatorActivity extends Activity {
         final ArrayList<String> languagesArrayList = new ArrayList<>();
         languagesArrayList.add("ru");
         languagesArrayList.add("en");
-        languagesArrayList.add("de");
-        languagesArrayList.add("es");
         languagesArrayList.add("fr");
-        languagesArrayList.add("it");
-        languagesArrayList.add("pt");
 
         sourceLangBtn = (Button) findViewById(R.id.source_language);
         sourceLangBtn.setOnClickListener(new View.OnClickListener() {
