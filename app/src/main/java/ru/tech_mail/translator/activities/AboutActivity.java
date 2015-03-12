6c96262ce74396a6aedbb666ffe37b814c5081bc
naro19 <naro91@mail.ru>
2015-03-12 15:04:40 +0300

    Страница About переделана на фрагментах и учитывает ориентацию

--- app/src/main/java/ru/tech_mail/translator/activities/AboutActivity.java
+++ app/src/main/java/ru/tech_mail/translator/activities/AboutActivity.java
@@ -2,13 +2,14 @@ package ru.tech_mail.translator.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
 import ru.tech_mail.translator.R;
 
 /**
  * Created by Abovyan on 06.03.15.
  */
-public class AboutActivity extends Activity {
+public class AboutActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
