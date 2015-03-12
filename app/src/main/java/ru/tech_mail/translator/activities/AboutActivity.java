44a5c1f9e44a44e850504b1ae09d06b8e82ecf61
naro19 <naro91@mail.ru>
2015-03-12 15:39:54 +0300

    Добавил ссылку на API

--- app/src/main/java/ru/tech_mail/translator/activities/AboutActivity.java
+++ app/src/main/java/ru/tech_mail/translator/activities/AboutActivity.java
@@ -1,8 +1,12 @@
 package ru.tech_mail.translator.activities;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
 
 import ru.tech_mail.translator.R;
 
@@ -14,5 +18,13 @@ public class AboutActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
+        Button startBrowser = (Button) findViewById(R.id.linkAPI);
+        startBrowser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://tech.yandex.ru/translate/"));
+                startActivity(browserIntent);
+            }
+        });
     }
 }
